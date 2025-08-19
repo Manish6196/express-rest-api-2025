1ad22efb93c87b9959f52653a2fe20a9de13ce24
manish <manishrajput6196@gmail.com>
2025-08-19 20:21:25 +0530

    Enhanced logging with winston

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,8 @@
 const config = {
   env: process.env.NODE_ENV || 'development',
-  debug: process.env.APP_DEBUG === 'true',
   port: parseInt(process.env.PORT || '3000'),
+  debug: process.env.APP_DEBUG === 'true',
+  logLevel: process.env.LOG_LEVEL || 'info',
   appSecret: process.env.APP_SECRET || '',
   issuerBaseUrl: process.env.ISSUER_BASE_URL || '',
   audience: process.env.AUDIENCE || '',
