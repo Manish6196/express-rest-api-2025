462e1dc3140a6e53ddd7a937c41049288648cfaf
manish <manishrajput6196@gmail.com>
2025-08-19 12:51:14 +0530

    Error handling

--- src/routes/v1/tasks/controller.ts
+++ src/routes/v1/tasks/controller.ts
@@ -1,9 +1,11 @@
 import { Request, Response } from 'express';
+import EntityNotFoundError from '../../../errors/EntityNotFoundError';
 
 export const listTasks = (req: Request, res: Response) => {
   res.status(200).json([]);
 };
 
 export const getTask = (req: Request, res: Response) => {
+  throw new EntityNotFoundError('Task not found', 404, 'ERR_NF');
   res.status(200).json({ id: 1, name: 'Task 1' });
 };
