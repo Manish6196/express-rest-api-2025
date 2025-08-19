2b264066a736d8a3d1c085baf9b4362f2d432599
manish <manishrajput6196@gmail.com>
2025-08-19 19:42:11 +0530

    Auth0 Integration

--- src/middleware/error-handler.ts
+++ src/middleware/error-handler.ts
@@ -2,6 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 import config from '../config';
 import { getErrorMessage } from '../utils';
 import CustomError from '../errors/CustomError';
+import { UnauthorizedError } from 'express-oauth2-jwt-bearer';
 
 export default function errorHandler(
   error: unknown,
@@ -19,6 +20,16 @@ export default function errorHandler(
       error: {
         message: error.message,
         code: error.code,
+      },
+    });
+    return;
+  }
+
+  if (error instanceof UnauthorizedError) {
+    res.status(401).json({
+      error: {
+        message: error.message,
+        code: 'code' in error ? error.code : 'ERR_AUTH',
       },
     });
     return;
