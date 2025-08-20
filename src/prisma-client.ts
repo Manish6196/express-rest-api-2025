1f83a45ed55c79191a1c61fd8c7daa7f8fa5eb30
manish <manishrajput6196@gmail.com>
2025-08-20 13:54:31 +0530

    Root path alias

--- src/prisma-client.ts
+++ src/prisma-client.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '../prisma-client'; // Plese run `npm run prisma:generate` to generate the Prisma client
+import { PrismaClient } from '@root/prisma-client'; // Plese run `npm run prisma:generate` to generate the Prisma client
 
 const prisma = new PrismaClient();
 
